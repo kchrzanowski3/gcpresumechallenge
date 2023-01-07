faf8f6940b49d4a9641c721b42154bd74e6cefa7
kchrzanowski3 <kyle.chrzanowski@me.com>
2023-01-07 12:21:34 -0500

    a

--- Front End Files/scripts.js
+++ Front End Files/scripts.js
@@ -1,4 +1,4 @@
-fetch('https://d503n28gj5.execute-api.us-east-1.amazonaws.com/')
+fetch('https://apigateway10-7gxty8ef.ue.gateway.dev/getvisitors')
     .then(res => res.json())
     .then(res => {document.getElementById("counter").innerHTML = res.body.count})
     .then(data => console.log(data));
