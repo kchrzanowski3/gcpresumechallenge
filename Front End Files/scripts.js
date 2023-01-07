7ee04487cd57ea03fd909b81933f60e9fe566789
kchrzanowski3 <kyle.chrzanowski@me.com>
2023-01-07 12:32:21 -0500

    push updated apigateway

--- Front End Files/scripts.js
+++ Front End Files/scripts.js
@@ -1,4 +1,4 @@
 fetch('https://apigateway10-7gxty8ef.ue.gateway.dev/getvisitors')
     .then(res => res.json())
     .then(res => {document.getElementById("counter").innerHTML = res.body.count})
-    .then(data => console.log(data));
+    .then(data => console.log(data)); 
