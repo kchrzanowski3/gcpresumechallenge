15387d3a3a008e60ccd9d474fde47991b3569627
kchrzanowski3 <kyle.chrzanowski@me.com>
2023-02-11 16:07:56 -0500

    cypress update
    Added cypress and npm functionality into the project and updated the front end workflow to test it.

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,39 @@
+let countertext1
+let countertext2
+
+describe('tests the website is up and the API is working', () => {
+
+  it('Visits kylechrzanowski.com and gets the visitor count', () => {
+    cy.visit('https://kylechrzanowski.com')
+    cy.contains('Kyle Chrzanowski')
+    cy.get('[id=counter]')
+        .invoke('text')
+        .should('match', /^[0-9]*$/)
+    cy.get('[id=counter]').then(($counter) => {
+      // save text from the first element
+      countertext1 = $counter.text()
+    })
+  })
+
+
+  it('reloads and checks the updated visitor count has increased by 1', () => {
+    cy.visit('https://kylechrzanowski.com')
+    cy.get('[id=counter]')
+        .invoke('text')
+        .should('match', /^[0-9]*$/)
+    cy.get('[id=counter]').then(($counter) => {
+      // save text from the first element
+      countertext2 = $counter.text()
+
+      expect(String(Number(countertext1)+1)).to.equal(countertext2)
+    })
+  })
+
+
+
+})
+
+
+
+
+
