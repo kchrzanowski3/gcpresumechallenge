9242d3c33ec4a4acbe4872731d4365cc18e5e5d3
kchrzanowski3 <kyle.chrzanowski@me.com>
2023-02-13 14:20:41 -0500

    a

--- terraform/publish-artifacts/artifact-pub.tf
+++ terraform/publish-artifacts/artifact-pub.tf
@@ -5,6 +5,17 @@ terraform {
       version = "4.51.0"
     }
   }
+  cloud {
+    organization = "kylechrzanowski"
+
+    workspaces {
+      name = "gcp-resume-build"
+    }
+  }
+}
+
+terraform {
+
 }
 
 provider "google" {
@@ -13,21 +24,9 @@ provider "google" {
   zone        = "us-east1-b"
 }
 
-provider "google-beta" {
-  project     = "strange-cycle-371319"
-  region      = "us-east1"
-  zone        = "us-east1-b"
-}
-
-//upload the zipped up file to a bucket
+//create the bucket for code to be uploaded to
 resource "google_storage_bucket" "codebucket" {
   name     = "resume-function-code-bucket"
   location = "US"
   uniform_bucket_level_access = true
-}
-
-resource "google_storage_bucket_object" "archive" {
-  name   = "code.zip"
-  bucket = google_storage_bucket.codebucket.name
-  source = "../../terraform/build/built-code/code.zip"
 }
