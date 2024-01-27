02efa73b203fa1c2253012950b2372f19ad75fcc
Kyle Chrzanowski <kyle.chrzanowski@gmail.com>
2024-01-27 20:46:31 +0000

    asdf

--- oidc-idp/enabled-apis.tf
+++ oidc-idp/enabled-apis.tf
@@ -8,9 +8,10 @@ locals {
     "iam.googleapis.com",                  # Identity and Access Management (IAM) API
     "iamcredentials.googleapis.com",       # IAM Service Account Credentials API
     "cloudresourcemanager.googleapis.com", # Cloud Resource Manager API
-    "sts.googleapis.com",                  # Security Token Service API
+    "sts.googleapis.com",  # Security Token Service API
+    "serviceusage.googleapis.com",                #Enables services that service consumers want to use on Google Cloud Platform, lists the available or enabled services, or disables services that service consumers no longer use.
 
-    # You can add more apis to enable in the project
+
   ])
 }
 
