ff380fa548d85a91d2c194ed8c3918d702b820a9
Kyle Chrzanowski <kyle.chrzanowski@gmail.com>
2024-01-30 22:34:57 +0000

    fix xss vulnerability

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 fetch('https://kyle-resume-api-gateway-4jnr4ia3.ue.gateway.dev/getvisitors')
     .then(res => res.json())
     .then(res => {document.getElementById("counter").innerHTML = res.body.count})
+    .then(res => {
+        let count = res.body.count;
+      
+        // Check if the count is a number, if not, return a default value
+        if (typeof count !== 'number' || isNaN(count)) { 
+          count = 'ERROR (NUMBER NOT RETURNED)'; // Set a default value of 0 if it's not a valid number
+        }
+      
+        document.getElementById("counter").innerHTML = count; 
+      })
     .then(data => console.log(data));
