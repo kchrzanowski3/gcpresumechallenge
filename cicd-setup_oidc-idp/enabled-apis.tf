711aa37290d0bb39c76daf7f0de16eec0e22d0e2
Kyle Chrzanowski <kyle.chrzanowski@gmail.com>
2024-02-04 16:29:02 +0000

    asdf

--- cicd-setup_oidc-idp/enabled-apis.tf
+++ cicd-setup_oidc-idp/enabled-apis.tf
@@ -1,30 +1,4 @@
 ##
-## apis for cicd auth and github actions and stuff stuff
-## 
-
-# locals {
-#   services = toset([
-#     # MUST-HAVE for GitHub Actions setup
-#     "iam.googleapis.com",                  # Identity and Access Management (IAM) API
-#     "iamcredentials.googleapis.com",       # IAM Service Account Credentials API
-#     "cloudresourcemanager.googleapis.com", # Cloud Resource Manager API
-#     "sts.googleapis.com",  # Security Token Service API
-#     "serviceusage.googleapis.com",                #Enables services that service consumers want to use on Google Cloud Platform, lists the available or enabled services, or disables services that service consumers no longer use.
-
-
-#   ])
-# }
-
-
-# resource "google_project_service" "service" {
-#   for_each = local.services
-#   project  = var.project
-#   service  = each.value
-#   disable_dependent_services = true
-#   #disable_on_destroy = false
-# }
-
-##
 ## Enabled APIs for the project where the OIDC idp provider is located
 ##
 
