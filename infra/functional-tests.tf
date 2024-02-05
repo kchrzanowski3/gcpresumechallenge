f7a6631e5633e5014b64fd813ad2d253d0fd1459
Kyle Chrzanowski <kyle.chrzanowski@gmail.com>
2024-02-05 16:32:17 +0000

    updated test file

--- infra/functional-tests.tf
+++ infra/functional-tests.tf
@@ -5,7 +5,7 @@
 #dynamically inject variables into the cypress spec.cy.js test file 
 resource "local_file" "rendered_cypress_test" {
   content = templatefile("${path.module}/cypress-test-template.cy.js.tpl", {
-    website_address = google_compute_global_address.https_public_ip.address
+    website_address = var.environment == "prod" ? "https://${var.domain}" : "http://${google_compute_global_address.https_public_ip.address}"  #tests an ip or the actual domain depending on prod or not
     api_address = google_api_gateway_gateway.api_gw.default_hostname
     api_path = local.api_path
   })
