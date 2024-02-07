bd26282f040d6b0e96c70edb17f639ba79e47b22
Kyle Chrzanowski <kyle.chrzanowski@gmail.com>
2024-02-07 03:38:25 +0000

    adding people also search expansion boxes

--- infra/scripts.js
+++ infra/scripts.js
@@ -1,4 +1,22 @@
-fetch('https://kyle-resume-api-gateway-3ti78e4q.ue.gateway.dev/getvisitors')
-    .then(res => res.json())
-    .then(res => {document.getElementById("counter").innerHTML = Number(res.body.count)})
-    .then(data => console.log(data));
+function totalvisitors() {
+    fetch('https://kyle-resume-api-gateway-3ti78e4q.ue.gateway.dev/getvisitors')
+        .then(res => res.json())
+        .then(res => {document.getElementById("counter").innerHTML = Number(res.body.count)})
+        .then(data => console.log(data));
+}
+
+
+
+
+function dropdowns() {
+    const questionBoxes = document.querySelectorAll('.question-box');
+
+    questionBoxes.forEach(box => {
+    box.addEventListener('click', () => {
+        const answer = box.querySelector('.answer');
+        answer.display = answer.display === 'block' ? 'none' : 'block';
+    });
+});
+}
+    
+    
