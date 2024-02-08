29777a6d9b9a69ed8fc69e24171c5aedab16bf07
Kyle Chrzanowski <kyle.chrzanowski@gmail.com>
2024-02-08 15:05:42 +0000

    update visitorcount set attribute

--- infra/scripts.js.tpl
+++ infra/scripts.js.tpl
@@ -1,7 +1,7 @@
 function totalvisitors() {
     fetch('${api_link}')
         .then(res => res.json())
-        .then(res => {document.getElementById("counter").innerHTML = Number(res.body.count)})
+        .then(res => {document.getElementById("visitorCount").innerHTML = Number(res.body.count)})
         .then(data => console.log(data));
 }
 
--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function totalvisitors() {
-    fetch('https://kyle-resume-api-gateway-3ti78e4q.ue.gateway.dev/getvisitors')
+    fetch('https://kyle-resume-api-gateway-6qm4k23f.ue.gateway.dev/getvisitors')
         .then(res => res.json())
         .then(res => {document.getElementById("visitorCount").innerHTML = Number(res.body.count)})
         .then(data => console.log(data));
