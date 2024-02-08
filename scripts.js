c065f680138e6f92c40665e6cff810250ac1edf7
Kyle Chrzanowski <kyle.chrzanowski@gmail.com>
2024-02-08 02:25:32 +0000

    updated js function to open and close about me dropdowns

---
+++ scripts.js
@@ -0,0 +1,25 @@
+function totalvisitors() {
+    fetch('https://kyle-resume-api-gateway-3ti78e4q.ue.gateway.dev/getvisitors')
+        .then(res => res.json())
+        .then(res => {document.getElementById("counter").innerHTML = Number(res.body.count)})
+        .then(data => console.log(data));
+}
+
+function dropdowns(event) {    
+    const questionBox = event.currentTarget;  
+    const answer = questionBox.querySelector('.answer');
+    const expandIcon = questionBox.querySelector('.expand-icon');
+
+    answer.classList.toggle('show')
+
+    //answer.classList.toggle('display'); // Toggle the visibility of the answer
+
+    // Rotate the expand icon
+    if (expandIcon.textContent === '+') {
+        expandIcon.textContent = '-';
+    } else {
+        expandIcon.textContent = '+';
+    }
+}
+    
+    
